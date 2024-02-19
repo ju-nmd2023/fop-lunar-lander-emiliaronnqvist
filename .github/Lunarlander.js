bf5b3c8a8d2b93578cfb6d3e16484bcaaaae2c18
emiliaronnqvist <emilia.ronnqvist@hotmail.com>
2024-02-19 10:17:51 +0100

    Update Lunarlander.js

--- .github/Lunarlander.js
+++ .github/Lunarlander.js
@@ -3,9 +3,12 @@ let x = 250;
 let y = 100;
 let velocity = 0;
 let acceleration = 0;
+let gameOver = false;
+// Following three lines was adapted from chat gpt
 let gravity = 0.12;
 let thrust = 0.6;
 let victory = false;
+
 noStroke();
 createCanvas(500, 500);
 
@@ -39,11 +42,33 @@ function flames(x, y) {
   fill(255, 255, 0);
   ellipse(x, y + random(35, 50), 15, 40);
 }
+// Game over page
+function endPage(x, y) {
+  background(0);
+  fill(250);
+  textAlign(CENTER, CENTER);
+  textSize(40);
+  text("You crashed!", x, y);
+  textSize(15);
+  text("PRESS ENTER TO RESTART GAME", x, y + 50);
+}
+// Start page
+function startPage(x, y) {
+  background(255);
+  textAlign(CENTER, CENTER);
+  textSize(40);
+  text("Lunar Lander", x, y);
+  textSize(15);
+  text("PRESS ENTER TO START", x, y + 50);
+}
+
+//Victory page
 
 function draw() {
   if (started) {
     // background
     background(0);
+    // following 6 lines was adapted from chat gpt
     if (!victory) {
       flames(x, y);
     }
@@ -51,41 +76,43 @@ function draw() {
     velocity += acceleration;
     y += velocity;
   } else {
-    //start page
-    background(255);
-    textAlign(CENTER, CENTER);
-    textSize(40);
-    text("Lunar Lander", x, y);
-    textSize(40);
-    text("Press enter to start", x, y + 50);
+    startPage(x, y);
   }
-  // startin game by pressing enter
-  if (keyPressed(13)) {
+  // Starting the game by pressing enter
+  // Following 1 line below was adapted from chat gpt
+  if (keyIsPressed && keyCode === ENTER) {
     started = true;
   }
-  // making the rocket fly with thrust
+  // Making the rocket fly with thrust
   if (keyIsDown(40) && y < 450) {
+    // following 1 line below was adapted from chat pgt
     acceleration = -thrust;
     // adding flames
     victory = false;
   } else {
-    // if key 40 not pressed, gravity
+    // If key 40 is not pressed, there will be gravity
+    // following 1 line below was adapted from chat pgt
     acceleration = gravity;
   }
-  // Rocket landed with velocity below 1.5
+  // Show endPage if velocity higher than 1.5
+  if (gameOver) {
+    endPage(x, y);
+  }
+  // Rocket will land correctly with velocity below 1.5
   if (y >= 450 && velocity < 1.5) {
     y = 450;
     velocity = 0;
     // remove flames
     victory = true;
+  } else if (y >= 450 && velocity > 1.5) {
+    gameOver = true;
+    started = false;
   }
 }
 
-//button
-/*
-function button(x, y, w, h) {
-    rect(x, y ,w ,h);
+if (!started && keyIsPressed && keyCode === ENTER) {
+  started = true;
+} else if (gameOver && keyIsPressed && keyCode === ENTER) {
+  started = true;
+  gameOver = false;
 }
-*/
-
-// Stars
