99a80921b2d555cf9f63e46488874c36710d1f8d
emiliaronnqvist <emilia.ronnqvist@hotmail.com>
2024-02-22 20:15:08 +0100

    final

--- .github/Lunarlander.js
+++ .github/Lunarlander.js
@@ -6,14 +6,16 @@ let gameOver = false;
 let started = false;
 let restart = false;
 // Following three lines was adapted from chat gpt
-let gravity = 0.12;
+let gravity = 0.11;
 let thrust = 0.6;
-let victory = false;
+let flamesOn = false;
 
 noStroke();
 createCanvas(500, 500);
+textStyle(BOLD);
+textAlign(CENTER, CENTER);
 
-// Following 28 lines are inspired by following youtube-tutorial https://www.youtube.com/watch?v=cl5FW_zgY_Q
+// Following 28 lines was inspired by following youtube-tutorial https://www.youtube.com/watch?v=cl5FW_zgY_Q
 function rocket(x, y) {
   // side fins
   fill(194, 116, 185);
@@ -43,88 +45,148 @@ function flames(x, y) {
   fill(255, 255, 0);
   ellipse(x, y + random(35, 50), 15, 40);
 }
-// Game over page
+
+// Game Over page
 function endPage(x, y) {
-  background(0);
-  fill(255, 255, 255);
-  textAlign(CENTER, CENTER);
+  // background and text
+  background(21, 19, 46);
   textSize(40);
+  fill(239, 210, 250);
   text("You crashed!", x, y);
   textSize(15);
+  fill(243, 139, 252);
   text("PRESS ENTER TO RESTART GAME", x, y + 50);
+
+  // body & top crashed
+  push();
+  translate(160, 200);
+  rotate(1.1);
+  // body
+  fill(170, 111, 199);
+  ellipse(x, y, 30, 80);
+  // top
+  fill(194, 116, 185);
+  beginShape();
+  vertex(287, 470);
+  arc(x, y - 15, 26, 50, PI, 0);
+  // window missing
+  fill(21, 19, 46);
+  ellipse(x, y - 10, 18, 20);
+  ellipse(x + 4, y - 10, 5, 8);
+  pop();
+  // window crashed
+  fill(190, 198, 204);
+  ellipse(x + 30, y + 380, 18, 20);
+  // side fins
+  fill(194, 116, 185);
+  arc(x + 100, y + 380, 60, 40, TWO_PI, PI / 2);
+  push();
+  translate(-10, 200);
+  rotate(1, 2);
+  arc(x, y, 60, 40, TWO_PI, PI / 2);
+  pop();
 }
+
 // Start page
 function startPage(x, y) {
-  background(255);
-  fill(0);
-  textAlign(CENTER, CENTER);
+  // background and text
+  background(21, 19, 46);
+  fill(239, 210, 250);
   textSize(40);
   text("Lunar Lander", x, y);
   textSize(15);
+  fill(243, 139, 252);
   text("PRESS ENTER TO START", x, y + 50);
+  // Big rocket with flames
+  push();
+  translate(160, -250);
+  rotate(1);
+  scale(2);
+  flames(x, y);
+  rocket(x, y);
+  pop();
 }
+
 //Victory page
 function victoryPage(x, y) {
-  background(255);
-  fill(0);
-  textAlign(CENTER, CENTER);
+  // background and text
+  background(21, 19, 46);
   textSize(40);
+  fill(239, 210, 250);
   text("Rocket landed!", x, y);
   textSize(15);
-  text("PRESS ENTER RESTART", x, y + 50);
+  fill(243, 139, 252);
+  text("PRESS ENTER TO RESTART", x, y + 50);
+  // Rocket landed successfully
+  rocket(250, 450);
 }
-
+// Game when started
 function draw() {
   if (started) {
-    // background
-    background(0);
-    // following 6 lines was adapted from chat gpt
-    if (!victory) {
+    // Background
+    background(21, 19, 46);
+
+    // Draw flames
+    // following 2 lines was adapted from chat gpt
+    if (!flamesOn) {
       flames(x, y);
     }
-    rocket(x, y);
-    velocity += acceleration;
-    y += velocity;
 
-    // Starting the game by pressing enter
+    // Draw rocket
+    rocket(x, y);
+
+    // following 2 lines was adapted from chat gpt
+    // The rockets velocity changes over time because of the added acceleration
+    velocity = velocity + acceleration;
+    // The rockets y-coordinate changes over time because of velocity
+    y = y + velocity;
+
     // Making the rocket fly with thrust
     if (keyIsDown(40) && y < 450) {
       // following 1 line below was adapted from chat pgt
       acceleration = -thrust;
       // adding flames
-      victory = false;
+      flamesOn = false;
     } else {
-      // If key 40 is not pressed, there will be gravity
+      // If key 40 is not pressed, there will be gravity & no flames
       // following 1 line below was adapted from chat pgt
       acceleration = gravity;
+      flamesOn = true;
     }
 
-    // Rocket will land correctly with velocity below 1.5
+    // Rocket will land correctly with velocity below 1.5 velocity
     if (y >= 450 && velocity < 1.5) {
       // show victory page
       restart = true;
       rocket(250, 450);
       // remove flames
-      victory = true;
+      flamesOn = true;
     }
     if (y >= 450 && velocity > 1.5) {
+      // stop the rocket from moving
       velocity = 0;
-      gameOver = true;
-      y = 100;
       acceleration = 0;
+      // Show the gameOver page
+      gameOver = true;
+      // Where the rocket should restart after gameOver
+      y = 100;
     }
   }
+  // if game not started, show startPage
   if (!started) {
     startPage(250, 100);
   }
+  // if gameOver show endpage
   if (gameOver) {
     endPage(250, 100);
   }
+  // if restart(victory) show victoryPage
   if (restart) {
     victoryPage(250, 100);
   }
 }
-// restart and start game
+// Restart and start game
+// following lines inspired from cheat sheets
 function keyPressed() {
   if (!started && keyCode === ENTER) {
     started = true;
